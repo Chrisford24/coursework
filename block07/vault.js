f9a10d005d3941bd5ad74ebe87a898bb9b1b5824
Chrisford24 <Christopher11ford@yahoo.com>
2023-04-02 12:50:59 -0500

    block10 added

---
+++ block07/vault.js
@@ -0,0 +1,19 @@
+// variables used for project
+var first, second, third;
+
+let myName = 4;
+myName = "Chris";
+
+// Create a message to the user
+console.log("You have received this message because you have been chosen to open an important vault. Here is the secret combination:")
+
+// assign the variables an equation. 
+first = 100/10;
+second = 20 * 2;
+third = 139 % 100;
+
+
+// Display the combination to the user
+alert(first + " - " + second + " - "+ third);
+console.log(first + " - " + second + " - "+ third);
+// expected output: 10 - 40 - 39
---
+++ typed/java.js
@@ -0,0 +1,4 @@
+var typed = new Typed('.typed', {
+    strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+    typeSpeed: 50,
+});
