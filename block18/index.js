b2038e3b74db24dc0cfab10fa971e3a3e8f799d7
Chrisford24 <Christopher11ford@yahoo.com>
2023-05-08 17:24:54 -0500

    updated

---
+++ block18/index.js
@@ -0,0 +1,23 @@
+/*
+
+Unit Tests 
+
+Prompt 1 - A function called "concatOdds" takes two arrays of integers as arguments. It should return a single array that only contains the odd numbers, in ascending order, from both of the arrays.
+
+Prompt 1 Tests - 
+
+1. concatOdds([3, 2, 1], [9, 1, 1, 1, 4, 15, -1]) => [-1, 1, 3, 9, 15]
+
+2.concatOdds([...
+
+Prompt 2 - ......
+
+1. ...
+
+2. ...
+
+Functional Tests
+
+...
+
+*/
